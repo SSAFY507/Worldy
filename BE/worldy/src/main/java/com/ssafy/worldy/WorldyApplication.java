84bd60bc94435fa4ef7f23b2529f3320eb10635d
김설희 <2750seolhee@naver.com>
2023-04-14 17:36:55 +0900

    [FEAT] Spring 프로젝트 생성

---
+++ BE/worldy/src/main/java/com/ssafy/worldy/WorldyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.worldy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldyApplication.class, args);
+	}
+
+}
