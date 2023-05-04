a59fb13d8e54880418ebff9b95c9422c87bf1c8b
김설희 <2750seolhee@naver.com>
2023-05-04 14:13:31 +0900

    [UPDATE] 특정 유저가 스크랩한 모든 퀴즈 조회 기능 수정

--- BE/worldy/src/main/java/com/ssafy/worldy/model/user/service/UserQuizService.java
+++ BE/worldy/src/main/java/com/ssafy/worldy/model/user/service/UserQuizService.java
@@ -1,17 +1,23 @@
 package com.ssafy.worldy.model.user.service;
 
+import com.ssafy.worldy.exception.CustomException;
+import com.ssafy.worldy.exception.CustomExceptionList;
 import com.ssafy.worldy.model.quiz.dto.MultiAnswerDto;
 import com.ssafy.worldy.model.quiz.dto.QuizLikeDto;
+import com.ssafy.worldy.model.quiz.dto.QuizRecordDto;
 import com.ssafy.worldy.model.quiz.entity.MultiAnswer;
 import com.ssafy.worldy.model.quiz.entity.QuizLike;
+import com.ssafy.worldy.model.quiz.entity.QuizRecord;
 import com.ssafy.worldy.model.quiz.repo.MultiAnswerRepo;
 import com.ssafy.worldy.model.quiz.repo.QuizLikeRepo;
+import com.ssafy.worldy.model.quiz.repo.QuizRecordRepo;
 import com.ssafy.worldy.model.user.dto.ScrapDto;
 import com.ssafy.worldy.model.user.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,6 +33,9 @@ public class UserQuizService {
     UserRepo userRepo;
 
     @Autowired
+    QuizRecordRepo quizRecordRepo;
+
+    @Autowired
     KakaoUserService kakaoUserService;
 
     // 해당 유저의 찜 목록 조회
@@ -38,8 +47,9 @@ public class UserQuizService {
         List<QuizLikeDto> quizLikeDtoList = quizLikeList.stream().map(QuizLikeDto::new).collect(Collectors.toList());
         List<ScrapDto> scrapDtoList = quizLikeDtoList.stream().map(ScrapDto::new).collect(Collectors.toList());
 
-        // 객관식 문항에 대해서 모든 선택지 가져오기
+
         for(ScrapDto scrapDto: scrapDtoList) {
+            // 객관식 문항에 대해서 모든 선택지 가져오기
             if(scrapDto.getQuizType().equals("multi")) {
                 List<MultiAnswer> multiAnswerList = multiAnswerRepo.findByQuizId(scrapDto.getQuizId());
                 List<MultiAnswerDto> multiAnswerDtoList = multiAnswerList.stream().map(MultiAnswerDto::new).collect(Collectors.toList());
@@ -57,10 +67,20 @@ public class UserQuizService {
                     }
                 }
             }
+
+            // 해당 유저의 퀴즈 정답 여부를 확인하고, 틀렸을 경우 유저가 입력한 답안을 가져오기
+            Optional<QuizRecord> quizRecord = quizRecordRepo.findByQuizIdAndUserId(scrapDto.getQuizId(), userId);
+            if(quizRecord.isEmpty()) {
+                throw new CustomException(CustomExceptionList.QUIZ_RECORD_NOT_FOUND);
+            }
+
+            QuizRecordDto quizRecordDto = quizRecord.get().toDto();
+            scrapDto.setSuccess(quizRecordDto.isSuccess());
+            if(!quizRecordDto.isSuccess()) {
+                scrapDto.setUserAnswer(quizRecordDto.getUserAnswer());
+            }
         }
 
         return scrapDtoList;
     }
-
-
 }
