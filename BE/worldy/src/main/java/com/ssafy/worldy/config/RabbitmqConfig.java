8ba76ce8797811422100a638a5d242df505a7d70
김설희 <2750seolhee@naver.com>
2023-05-07 15:58:42 +0900

    [UPDATE] 소셜 로그인 경로 수정

--- BE/worldy/src/main/java/com/ssafy/worldy/config/RabbitmqConfig.java
+++ BE/worldy/src/main/java/com/ssafy/worldy/config/RabbitmqConfig.java
@@ -15,8 +15,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 @Slf4j
 public class RabbitmqConfig {
