36d2994fed478592b89d9e899382d6ce39955525
mihee78952 <mihee78952@naver.com>
2023-05-09 15:11:27 +0900

    [UPDATE] socket 수정

--- BE/worldy/src/main/java/com/ssafy/worldy/model/user/service/KakaoUserService.java
+++ BE/worldy/src/main/java/com/ssafy/worldy/model/user/service/KakaoUserService.java
@@ -47,7 +47,7 @@ public class KakaoUserService {
 
         // 1. 인가코드로 access 토큰 요청
         String accessToken = getAccessToken(code, "https://k8a507.p.ssafy.io/user/kakao/callback");
-//         String accessToken = getAccessToken(code, "http://localhost:3000/user/kakao/callback");
+      //   String accessToken = getAccessToken(code, "http://localhost:3000/user/kakao/callback");
 
         // 2. 없는 회원의 경우 회원가입
         User user = registKakaoUser(accessToken);
