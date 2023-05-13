ef3f52fe3589ab7ba2febc7eb2c3daaebc5296c3
mihee78952 <mihee78952@naver.com>
2023-05-13 16:26:51 +0900

    [UPDATE] 소켓 통신 수정
    Former-commit-id: 11b9c29c2ec658236bb58827b06682bf3997043c

---
+++ BE/worldy/src/main/java/com/ssafy/worldy/model/game/dto/MetaData.java
@@ -0,0 +1,20 @@
+package com.ssafy.worldy.model.game.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class MetaData {
+
+    private String roomId;
+    private String type = "metaData";
+    private int currentLocation;
+    private int dice1;
+    private int dice2;
+    private int dice;
+    private int turn;
+    private boolean isDouble;
+}
