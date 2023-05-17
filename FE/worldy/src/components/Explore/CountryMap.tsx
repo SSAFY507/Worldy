2744d0d1c1ae504b3894939f90625bd276bb1c0e
김설희 <2750seolhee@naver.com>
2023-05-17 18:23:36 +0900

    [UPDATE] 스페인 디테일 맵
    Former-commit-id: 5374b565dece995c4762f2f5d22c182c78f08a28

--- FE/worldy/src/components/Explore/CountryMap.tsx
+++ FE/worldy/src/components/Explore/CountryMap.tsx
@@ -20,7 +20,7 @@ import bg from '../../assets/images/WorldBackground.jpg'
 import { current } from '@reduxjs/toolkit';
 import europe_France from '../../assets/lowpoly/Country_America.glb';
 import europe_Italia from '../../assets/lowpoly/Country_America.glb';
-import europe_Spain from '../../assets/lowpoly/Country_America.glb';
+import europe_Spain from '../../assets/lowpoly/Country_Spain.glb';
 import europe_UK from '../../assets/lowpoly/Country_UK.glb';
 import northAmerica_America from '../../assets/lowpoly/Country_America.glb';
 import { useNavigate } from 'react-router';
