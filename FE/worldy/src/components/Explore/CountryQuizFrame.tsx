451a19cce20f50d853c933ea9c4947f23b8d099d
gnslalsl12 <wjdgnsxhsl@naver.com>
2023-05-19 01:55:21 +0900

    [FEAT] #01 add Animation to Count and fix Font of Quiz and Result
    Former-commit-id: 4fd9d9074de7196f8e4801f08a28f4ee525564bc

--- FE/worldy/src/components/Explore/CountryQuizFrame.tsx
+++ FE/worldy/src/components/Explore/CountryQuizFrame.tsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-import CountryPaintDetailModal from './CountryPaintDetailModal'
-import CountryQuizDetailModal from './CountryQuizDetailModal'
-import CustomAxios from '../../API/CustomAxios'
-import { ReactComponent as GOne } from "../../assets/images/1.svg"
-import { ReactComponent as GStart } from "../../assets/images/START.svg"
-import { ReactComponent as GThree } from "../../assets/images/3.svg"
-import { ReactComponent as GTwo } from "../../assets/images/2.svg"
-import { ReactComponent as POne } from "../../assets/images/p1.svg"
-import { ReactComponent as PStart } from "../../assets/images/pSTART.svg"
-import { ReactComponent as PThree } from "../../assets/images/p3.svg"
-import { ReactComponent as PTwo } from "../../assets/images/p2.svg"
-import { ReactComponent as PaintTitleIcon } from '../../assets/images/painttitle.svg'
-import QuizModal from '../QuizModal'
-import { ReactComponent as QuizTitleIcon } from "../../assets/images/quiztitle.svg"
-import { ScrappedQuizType } from '../../routes/MyPage'
-import { countryLst } from './CountrySpeak'
-import { useParams } from 'react-router'
+import CountryPaintDetailModal from './CountryPaintDetailModal';
+import CountryQuizDetailModal from './CountryQuizDetailModal';
+import CustomAxios from '../../API/CustomAxios';
+import { ReactComponent as GOne } from '../../assets/images/1.svg';
+import { ReactComponent as GStart } from '../../assets/images/START.svg';
+import { ReactComponent as GThree } from '../../assets/images/3.svg';
+import { ReactComponent as GTwo } from '../../assets/images/2.svg';
+import Count1Blue from '../../assets/images/Count1Blue.png';
+import Count2Blue from '../../assets/images/Count2Blue.png';
+import Count3Blue from '../../assets/images/Count3Blue.png';
+import StartBlue from '../../assets/images/StartBlue.png';
+
+import Count1Purple from '../../assets/images/Count1Purple.png';
+import Count2Purple from '../../assets/images/Count2Purple.png';
+import Count3Purple from '../../assets/images/Count3Purple.png';
+import StartPurple from '../../assets/images/StartPurple.png';
+import { ReactComponent as POne } from '../../assets/images/p1.svg';
+import { ReactComponent as PStart } from '../../assets/images/pSTART.svg';
+import { ReactComponent as PThree } from '../../assets/images/p3.svg';
+import { ReactComponent as PTwo } from '../../assets/images/p2.svg';
+import PaintTitleIcon from '../../assets/images/QuizPurple.png';
+import QuizModal from '../QuizModal';
+import QuizTitleIcon from '../../assets/images/QuizBlue.png';
+import { ScrappedQuizType } from '../../routes/MyPage';
+import { countryLst } from './CountrySpeak';
+import { useParams } from 'react-router';
+import QuizTextP from '../../assets/images/QuizPurple.png';
+import QuizTextB from '../../assets/images/QuizBlue.png';
+
+import '../../styles/CountryQuizFrameStyles.css';
 
 // const tempScrappedQuizList: ScrappedQuizType[] = [
 //   {
@@ -98,59 +111,63 @@ import { useParams } from 'react-router'
 //   },
 // ];
 
-
 interface Props {
   selectAsset: string;
-};
+}
 
 export interface PaintDataType {
-  imgNum: number,
-  diffUrl: string,
-  imgTitle: string,
-  imgSubTitle: string,
-  imgContent: string,
-  originalUrl: string,
-  answerPointList: string[][]
-};
+  imgNum: number;
+  diffUrl: string;
+  imgTitle: string;
+  imgSubTitle: string;
+  imgContent: string;
+  originalUrl: string;
+  answerPointList: string[][];
+}
 
 export interface QuizDataType {
-    quizId: number,
-    nation: {
-        ationId: number,
-        nationName: string
-    },
-    publisherType: string,
-    quizType: string,
-    category: string,
-    level: number,
-    image: string,
-    content: string,
-    answer: string,
-    hint: string,
-    hintType: boolean,
-    commentary: string,
-    report: number,
-    multiAnswerList: {
-      multiAnswerId: number,
-      quizDto: any,
-      answer: string,
-      num: number,
-    }[]| null
-};
-
+  quizId: number;
+  nation: {
+    ationId: number;
+    nationName: string;
+  };
+  publisherType: string;
+  quizType: string;
+  category: string;
+  level: number;
+  image: string;
+  content: string;
+  answer: string;
+  hint: string;
+  hintType: boolean;
+  commentary: string;
+  report: number;
+  multiAnswerList:
+    | {
+        multiAnswerId: number;
+        quizDto: any;
+        answer: string;
+        num: number;
+      }[]
+    | null;
+}
 
 interface ImgObjectType {
-  [key:string]: JSX.Element[];
-};
+  [key: string]: JSX.Element[];
+}
 
-const DOMAIN = process.env.REACT_APP_BASE_URL
+const DOMAIN = process.env.REACT_APP_BASE_URL;
 
-const CountryQuizFrame = ({selectAsset}:Props) => {
-  const [counting, setCounting] = useState<number>(-2)
+const CountryQuizFrame = ({ selectAsset }: Props) => {
+  const [counting, setCounting] = useState<number>(-2);
   const [quizModalState, setQuizModalState] = useState<boolean>(false);
   const [selectedQuizId, setSelectedQuizId] = useState<number>(0);
-  const [axiosGetPaintData, setAxiosGetPaintData] = useState<PaintDataType | undefined>();
-  const [axiosGetQuizData, setAxiosGetQuizData] = useState<QuizDataType[] | undefined>();
+  const [axiosGetPaintData, setAxiosGetPaintData] = useState<
+    PaintDataType | undefined
+  >();
+  const [axiosGetQuizData, setAxiosGetQuizData] = useState<
+    QuizDataType[] | undefined
+  >();
 
   const params = useParams();
   const countryName: string = params.country || '';
@@ -159,52 +176,52 @@ const CountryQuizFrame = ({selectAsset}:Props) => {
   const countryId = countryLst[countryName].id;
 
   /** 게임을 재시작 하는 함수 */
-  const GetRegameFlag = (num:number) => {
+  const GetRegameFlag = (num: number) => {
     setCounting(num);
     if (countryId) {
-      if (selectAsset === "quizBox") {
-        getDatasList("quizBox", `/quiz/nation/${countryId}`);
+      if (selectAsset === 'quizBox') {
+        getDatasList('quizBox', `/quiz/nation/${countryId}`);
       } else {
-        getDatasList("paintBox", `/quiz/hidden/${countryId}`);
-      }          
+        getDatasList('paintBox', `/quiz/hidden/${countryId}`);
+      }
     }
   };
 
-  const imgObject:ImgObjectType = {
-    quizBox:[
+  const imgObject: ImgObjectType = {
+    quizBox: [
       <QuizTitleIcon />,
-      <GOne />, 
-      <GTwo />, 
-      <GThree />, 
+      <GOne />,
+      <GTwo />,
+      <GThree />,
       <GStart />,
-      <CountryQuizDetailModal 
+      <CountryQuizDetailModal
         selectAsset={selectAsset}
         GetRegameFlag={GetRegameFlag}
         axiosGetQuizData={axiosGetQuizData}
-      /> 
+      />,
       // <QuizModal
       //   input={tempScrappedQuizList[selectedQuizId]}
       //   closeModal={() => setQuizModalState(false)}
       // />
     ],
-    paintBox:[
-      <PaintTitleIcon />, 
-      <POne />, 
-      <PTwo />, 
-      <PThree />, 
+    paintBox: [
+      <PaintTitleIcon />,
+      <POne />,
+      <PTwo />,
+      <PThree />,
       <PStart />,
       <CountryPaintDetailModal
         selectAsset={selectAsset}
         GetRegameFlag={GetRegameFlag}
         axiosGetPaintData={axiosGetPaintData}
-      />
+      />,
     ],
-  }
+  };
 
-  const selectImg:JSX.Element[] = imgObject[selectAsset];
+  const selectImg: JSX.Element[] = imgObject[selectAsset];
 
   /** 데이터 받는 함수 */
-  const getDatasList = async (box: string, url:string) => {
+  const getDatasList = async (box: string, url: string) => {
     try {
       const response = await CustomAxios({
         APIName: 'getDatasList',
@@ -212,85 +229,138 @@ const CountryQuizFrame = ({selectAsset}:Props) => {
         UrlQuery: DOMAIN + url,
         Token: getLoginToken,
       });
-      if (box === "quizBox") {
-          setAxiosGetQuizData(response);
+      if (box === 'quizBox') {
+        setAxiosGetQuizData(response);
       } else {
         setAxiosGetPaintData(response);
-      }          
+      }
     } catch (error) {
       console.log('Error fetching data:', error);
     }
-  }
+  };
 
   useEffect(() => {
     if (countryId) {
-      if (selectAsset === "quizBox") {
-        getDatasList("quizBox", `/quiz/nation/${countryId}`);
+      if (selectAsset === 'quizBox') {
+        getDatasList('quizBox', `/quiz/nation/${countryId}`);
       } else {
-        getDatasList("paintBox", `/quiz/hidden/${countryId}`);
-      }          
+        getDatasList('paintBox', `/quiz/hidden/${countryId}`);
+      }
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (counting > -1) {
       setTimeout(() => {
-        setCounting((i) => i-1)
+        setCounting((i) => i - 1);
       }, 1000);
     }
-  },[counting])
+  }, [counting]);
 
   return (
-    <div className="h-full w-full mt-20 flex  items-center justify-center">
-      {(counting > -2)
-        ?
-        <div className="h-full w-full flex items-center justify-center ">
-          {(counting === 3) ? <div className="absolute">{selectImg[3]}</div> : null}
-          {(counting === 2) ? <div className="absolute">{selectImg[2]}</div> : null}
-          {(counting === 1) ? <div className="absolute">{selectImg[1]}</div> : null}
-          {(counting === 0) ? <div className="absolute">{selectImg[4]}</div> : null}
-          {(counting === -1 && (axiosGetPaintData || axiosGetQuizData) ) 
-            ?         
-            <div className={
-              `${(selectAsset === "quizBox") ? "absolute h-[1100px] flex" : "h-full w-full flex items-center justify-center"} `} 
+    <div className='h-full w-full mt-20 flex  items-center justify-center'>
+      {counting > -2 ? (
+        <div className='h-full w-full flex items-center justify-center '>
+          {counting === 3 ? (
+            selectAsset === 'quizBox' ? (
+              <img src={Count3Blue} alt='blue3' className='CQF-Counting' />
+            ) : (
+              <img src={Count3Purple} alt='purple3' className='CQF-Counting' />
+            )
+          ) : null}
+          {counting === 2 ? (
+            selectAsset === 'quizBox' ? (
+              <img src={Count2Blue} alt='blue2' className='CQF-Counting' />
+            ) : (
+              <img src={Count2Purple} alt='purple2' className='CQF-Counting' />
+            )
+          ) : null}
+          {counting === 1 ? (
+            selectAsset === 'quizBox' ? (
+              <img src={Count1Blue} alt='blue1' className='CQF-Counting' />
+            ) : (
+              <img src={Count1Purple} alt='purple1' className='CQF-Counting' />
+            )
+          ) : null}
+          {counting === 0 ? (
+            selectAsset === 'quizBox' ? (
+              <img src={StartBlue} alt='STblue' className='CQF-Start' />
+            ) : (
+              <img src={StartPurple} alt='STpurple' className='CQF-Start' />
+            )
+          ) : null}
+          {counting === -1 && (axiosGetPaintData || axiosGetQuizData) ? (
+            <div
+              className={`${
+                selectAsset === 'quizBox'
+                  ? 'absolute h-[1100px] flex'
+                  : 'h-full w-full flex items-center justify-center'
+              } `}
             >
               {selectImg[5]}
-            </div>    
-            :
-            null
-          }    
-        </div>
-
-      :
-      <div className="h-full w-1/3 flex items-center justify-center" >
-        <div className="absolute translate-y-[-120%]">
-          {selectImg[0]}
-        </div>
-        <div className="h-1/4 w-full flex flex-col shadow-xl ">
-          <div className={`h-1/6 w-full  rounded-t-xl shadow-3xl
-            ${(selectAsset === "quizBox") ? "bg-[#61C7BB]" : "bg-[#EACFFF]"}`}
-           />
-          <div className="h-5/6 w-full flex flex-col rounded-b-xl bg-white shadow-xl">
-            <div className="h-1/2 w-full flex flex-col justify-center items-center pt-10">
-              <button
-                className={`h-[50px] w-[300px] flex justify-center items-center border-solid border-[1px] rounded-3xl shadow-lg text-xl
-                  ${(selectAsset === "quizBox") ? "bordedr-[#61C7BB] text-[#61C7BB]" : "bordedr-[#C5ACE5] text-[#C5ACE5]"}`}
-                onClick={() => {
-                  setCounting(3)
-                }}
-                >
-                START
-              </button>
             </div>
-            <div className={`h-1/3 w-full text-xs opacity-60 font-PtdLight text-center 
-              ${(selectAsset === "quizBox") ? "text-[#61C7BB]" : "text-[#C5ACE5]"} pt-5`}
-            >버튼을 누르면 3초 후 문제가 공개됩니다.
+          ) : null}
+        </div>
+      ) : (
+        <div className='h-full w-1/3 flex items-center justify-center '>
+          <div className='h-1/4 w-full flex flex-col shadow-xl  outline-black'>
+            <div
+              className={`h-1/6 w-full  rounded-t-xl shadow-3xl flex flex-col justify-end items-center
+               ${selectAsset === 'quizBox' ? 'bg-[#61C7BB]' : 'bg-[#EACFFF]'}`}
+            >
+              {/* <div
+              className={`h-1/6 w-full  rounded-t-xl shadow-3xl flex flex-col justify-end items-center
+              bg-[#EACFFF]`}
+            > */}
+              <div className='w-[200px]  outline-red-300 mb-[10px]'>
+                {selectAsset === 'quizBox' ? (
+                  <img src={QuizTextB} alt='QuizTextPurple' />
+                ) : (
+                  <img src={QuizTextP} alt='QuizTextPurple' />
+                )}
+              </div>
+            </div>
+            <div className='h-5/6 w-full flex flex-col rounded-b-xl bg-white shadow-xl'>
+              <div className='h-1/2 w-full flex flex-col justify-center items-center pt-10'>
+                <button
+                  className={`h-[50px] w-[300px] flex justify-center items-center border-solid border-[1px] rounded-3xl shadow-lg text-xl
+                  ${
+                    selectAsset === 'quizBox'
+                      ? 'bordedr-[#61C7BB] text-[#61C7BB]'
+                      : 'bordedr-[#C5ACE5] text-[#C5ACE5]'
+                  }`}
+                  onClick={() => {
+                    setCounting(3);
+                  }}
+                >
+                  {/* <button
+                  className={`h-[50px] w-[300px] flex justify-center items-center border-solid border-[1px] rounded-3xl shadow-lg text-xl
+                      bordedr-[#C5ACE5] text-[#C5ACE5]
+                  `}
+                  onClick={() => {
+                    setCounting(3);
+                  }}
+                > */}
+                  START
+                </button>
+              </div>
+              <div
+                className={`h-1/3 w-full text-[15px] opacity-70 font-PtdLight text-center 
+              ${
+                selectAsset === 'quizBox' ? 'text-[#61C7BB]' : 'text-[#C5ACE5]'
+              } pt-[30px]`}
+              >
+                {/* <div
+                className={`h-1/3 w-full text-[15px] opacity-70 font-PtdLight text-center text-[#b893e8]
+              pt-[30px]`}
+              > */}
+                버튼을 누르면 3초 후 문제가 공개됩니다.
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    }
+      )}
     </div>
-  )
-}
+  );
+};
 export default CountryQuizFrame;
