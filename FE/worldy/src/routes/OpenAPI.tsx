59546fff5ff1745da5f51c5a15eeb577fdb7e031
gnslalsl12 <wjdgnsxhsl@naver.com>
2023-05-23 13:36:58 +0900

    [FIX]
    Former-commit-id: c7f3ab7eae7e10f64cb8f132556f5b3aa7785233

--- FE/worldy/src/routes/OpenAPI.tsx
+++ FE/worldy/src/routes/OpenAPI.tsx
@@ -96,8 +96,8 @@ export default function OpenAPI() {
 
   const StartOpenAPI = () => {
     return (
-      <div className=' w-[95%] h-full outline-white flex flex-row justify-stretch items-start bg-[rgb(39,32,32)]'>
-        <div className='w-[50px] h-full bg-[#FF4D45]' />
+      <div className=' w-[95%] h-full outline-white flex flex-row justify-stretch items-start bg-[#0a0a26]'>
+        <div className='w-[50px] h-full bg-[#371fa1]' />
         <div className='py-[30px] px-[50px] h-full flex-1 flex flex-col justify-between items-start'>
           <span className='text-white font-PtdBold text-[40px]'>
             Open API 시작하기
@@ -122,7 +122,7 @@ export default function OpenAPI() {
   };
   const QuizOpenAPIContent = () => {
     return (
-      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-row justify-stretch items-start bg-[rgb(39,32,32)]'>
+      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-row justify-stretch items-start bg-[#0a0a26]'>
         <div
           className=' w-2/5  h-full p-[50px] flex flex-col justify-start items-start
         '
@@ -135,7 +135,7 @@ export default function OpenAPI() {
             도구와 BeautifulSoup 정적 크롤링을 활용하여 생성되었습니다.
           </span>
         </div>
-        <div className=' w-3/5 h-fit py-[20px] bg-[rgb(49,47,45)] flex flex-row justify-between '>
+        <div className=' w-3/5 h-fit py-[20px] bg-[#1e1c2f] flex flex-row justify-between '>
           <div className='w-[150px] h-full flex flex-col justify-start items-center'>
             <button className='rounded-[4px] w-[80px] h-[34px] bg-blue-500 text-white font-PtdRegular text-[16px] cursor-default'>
               활용 예시
@@ -153,7 +153,7 @@ export default function OpenAPI() {
 
   const QuizInfoGetContent = () => {
     return (
-      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-col justify-stretch items-start bg-[rgb(39,32,32)]'>
+      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-col justify-stretch items-start bg-[#0a0a26]'>
         <div className='w-full h-fit outline-white flex flex-col justify-start items-start py-[50px] px-[50px]'>
           <div className='w-full h-fit flex flex-row justify-start items-center border-0 border-b-[1.5px] border-b-[#4E4E4E] border-solid pb-[30px]'>
             <span className='text-white font-PtdMedium text-[35px] mr-[30px]'>
@@ -256,7 +256,7 @@ export default function OpenAPI() {
 
   const QuizRequestContent = () => {
     return (
-      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-col justify-stretch items-start bg-[rgb(39,32,32)]'>
+      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-col justify-stretch items-start bg-[#0a0a26]'>
         <div className='w-full h-fit outline-white flex flex-col justify-start items-start py-[50px] px-[50px]'>
           <div className='w-full h-fit flex flex-col justify-start items-start border-0 border-b-[1.5px] border-b-[#4E4E4E] border-solid pb-[20px]'>
             <div className='flex flex-row justify-start items-center text-[18px]  font-PtdRegular'>
@@ -264,7 +264,7 @@ export default function OpenAPI() {
               <span className='text-white'>getQuiz</span>
             </div>
             <div className='w-full h-[50px] my-[20px] flex flex-row justify-between items-center'>
-              <div className='flex-1 flex flex-row h-full justify-between bg-[#2c2c2c] rounded-md outline outline-[1px] outline-[#484848]'>
+              <div className='flex-1 flex flex-row h-full justify-between bg-[#2c2c2cb7] rounded-md outline outline-[1px] outline-[#484848]'>
                 <div className='flex flex-row h-full'>
                   <div className='w-[100px] h-full pl-[20px] border-0 border-r-[1px] border-solid border-[#484848] text-white font-PtdRegular flex flex-row justify-start items-center'>
                     GET
@@ -340,7 +340,7 @@ export default function OpenAPI() {
                 />
               </div>
               <input
-                className='w-[87%] h-full px-[20px] flex flex-row justify-start items-center bg-[#2c2c2c] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight placeholder-[#606060]'
+                className='w-[87%] h-full px-[20px] flex flex-row justify-start items-center bg-[#2c2c2cb7] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight placeholder-[#606060]'
                 placeholder='국가별 코드 숫자'
                 onChange={(e) => handleInputNationID(e)}
                 value={inputNationID}
@@ -351,7 +351,7 @@ export default function OpenAPI() {
                 quizType
               </span>
               <input
-                className='w-[87%] h-full px-[20px] flex flex-row justify-start items-center bg-[#2c2c2c] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight placeholder-[#606060]'
+                className='w-[87%] h-full px-[20px] flex flex-row justify-start items-center bg-[#2c2c2cb7] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight placeholder-[#606060]'
                 placeholder="객관식:'multi' / OX:'ox' / 빈칸채우기:'blank'"
                 onChange={(e) => handleInputQuizType(e)}
                 value={inputQuizType}
@@ -362,7 +362,7 @@ export default function OpenAPI() {
                 Category
               </span>
               <input
-                className='w-[87%] h-full px-[20px] flex flex-row justify-start items-center bg-[#2c2c2c] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight placeholder-[#606060]'
+                className='w-[87%] h-full px-[20px] flex flex-row justify-start items-center bg-[#2c2c2cb7] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight placeholder-[#606060]'
                 placeholder="문화,역사:'cul' / 시사,상식:'aff' / 기타:'etc'"
                 onChange={(e) => handleInputCategory(e)}
                 value={inputCategory}
@@ -384,7 +384,7 @@ export default function OpenAPI() {
 
   const QuizTestContent = () => {
     return (
-      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-col justify-stretch items-start bg-[rgb(39,32,32)]'>
+      <div className=' w-[95%] min-h-[100px] h-fit outline-white flex flex-col justify-stretch items-start bg-[#0a0a26]'>
         <div className='w-full h-fit outline-white flex flex-col justify-start items-start py-[50px] px-[50px]'>
           <div className='w-full h-fit flex flex-col justify-start items-start'>
             <span className='flex flex-row justify-start items-center text-[18px]  font-PtdRegular text-white'>
@@ -398,7 +398,7 @@ export default function OpenAPI() {
                 Header(8)
               </span>
             </div>
-            <div className='w-full min-h-[200px] h-fit flex flex-col justify-start items-start my-[20px] bg-[#2c2c2c] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight p-[10px]'>
+            <div className='w-full min-h-[200px] h-fit flex flex-col justify-start items-start my-[20px] bg-[#2c2c2cb7] rounded-md outline outline-[1px] outline-[#484848] text-white font-PtdLight p-[10px]'>
               <div className='w-full h-[30px] flex flex-row justify-between items-center'>
                 <button className='w-[60px] h-[30px] text-[12px] font-PtdLight rounded-md bg-[#212121]'>
                   JSON
