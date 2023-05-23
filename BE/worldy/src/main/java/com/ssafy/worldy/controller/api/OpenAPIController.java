713df93ddd17de06e4f7d5356640044e420fc4b2
김원규 <re9946@naver.com>
2023-05-23 12:44:36 +0900

    [FEAT] Open API 추가
    Former-commit-id: 30f3f21d765858263257fe6fcb5f3e3d62cd3a54

---
+++ BE/worldy/src/main/java/com/ssafy/worldy/controller/api/OpenAPIController.java
@@ -0,0 +1,25 @@
+package com.ssafy.worldy.controller.api;
+
+import com.ssafy.worldy.model.quiz.dto.OpenAPIQuizDto;
+import com.ssafy.worldy.model.quiz.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/get")
+@RequiredArgsConstructor
+@Slf4j
+public class OpenAPIController {
+    private final QuizService quizService;
+
+    @GetMapping("/quiz")
+    public ResponseEntity<OpenAPIQuizDto> getHiddenCatch(@RequestParam("nationId") Long nationId, @RequestParam("quizType") String quizType,
+                                                         @RequestParam("category") String category){
+
+        OpenAPIQuizDto openAPIQuizDto = quizService.getOpenAPIQuizDto(nationId, quizType, category);
+        return new ResponseEntity<>(openAPIQuizDto, HttpStatus.OK);
+    }
+}
